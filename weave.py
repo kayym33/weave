24be8f5d505710e9827b50f686036155282ddb3c
bwb23129 <kayla.mcdade.2023@uni.strath.ac.uk>
2026-03-02 20:23:46 +0000

    fixed issue with writing to the correct file, also fixed lists

--- weave.py
+++ weave.py
@@ -1,42 +1,49 @@
 """
 Python tool to convert a md file to html file
 """
-INDENT=(" " * 4)
+INDENT = (" " * 4)
+NL = "\n"
 # read in md file
 filepath="file.md"
 mdfile = open(filepath)
 outputstr = ""
-nl = "\n"
 
-for line in mdfile:
-    line = line.rstrip("\n")
-    if line.startswith("#"):
-        hashcount = 0
-        for char in line:
-            if char == "#":
-                hashcount += 1
-        newline = f"<h{hashcount}>" + line[hashcount+1:] + f"<h{hashcount}>" 
-        print(f"<h{hashcount}>" + line[hashcount+1:] + f"<h{hashcount}>")
-        outputstr += str(f"<h{hashcount}>" + line[hashcount+1:] + f"<h{hashcount}>" + nl)
-    
-    elif line.startswith("-"):
-        print("<lo>")
-        print(INDENT +"<li>" + line[2:] + "<li>")
-        print("<lo>\n")
-    else:
-        print("<p>" + line + "<p>")
+with open(filepath) as mdfile:
+    for line in mdfile:
+        line = line.rstrip("\n")
+        
+        if line.startswith("#"):
+            hashcount = 0
+            for char in line:
+                if char == "#":
+                    hashcount += 1
+                else:
+                    break
+            text = line[hashcount:].strip()
+            newline = f"<h{hashcount}>{text}</h{hashcount}>"
+            outputstr += newline + NL    
+        
+        elif line.startswith("-"):
+            newline = f"<li>{line[2:]}</li>"
+            outputstr += newline + NL
+        else:
+            newline = f"<p>{line}</p>"
+            outputstr += newline + NL
+
 
 # output to html file
+# random number just to test so i dont have to keep deleting the file if it already exists
 import random
+x = "newhtml" + str(random.randint(1111,9999)) 
+htmlfile = f"{x}.html"
 
-x = "newhtml" + str(random.randint(1111,9999))
-htmlfile = open(str(f"{x}.html"), "x")
-
-with open(str(htmlfile), "a") as filewriter:
+# create new file
+with open(htmlfile, "a") as filewriter:
     filewriter.write(outputstr)
     
-with open(str(htmlfile)) as f:
-    print(f.read())
+# print contents of file
+with open(htmlfile) as filereader:
+    print(filereader.read())
 
 # preferably be able to do:
 # - python weave.py "markdownfile.md" <- outputs file to same directory, default to same name as md file
